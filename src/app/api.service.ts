e8226e667f8b8890dd6fea5e0ac27bafe16e1ac6
monika.folatko <monika.folatko@ppuc.pl>
2022-04-11 09:20:36 +0200

    final version

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Joke } from './app.component';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
   public getJoke(): Observable<Joke> {
-    return this.http.get<Joke>('https://jokeserver111.herokuapp.com/joke');
+    return this.http.get<Joke>('https://norrisapp71.herokuapp.com/joke');
   }
 }
